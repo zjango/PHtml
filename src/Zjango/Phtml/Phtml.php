4bf8cebaab1389b132bcc27087ea60c445c93b44
zhu.yuanzhe <zhu.yuanzhe@zhimatech.com>
2015-04-19 20:31:46 +0800

    update pattern

--- src/Zjango/Phtml/Phtml.php
+++ src/Zjango/Phtml/Phtml.php
@@ -13,7 +13,7 @@ class Phtml {
 	 */
 
 	public static function init($html, $sType = 'Dom') {
-		$pattern='/^((http|https):\/\/)?[\w-_.]+(\/[\w-_]+)*\/?$/';
+		$pattern='/^(([a-zA-Z]+)(:\/\/))?([a-zA-Z]+)\.(\w+)\.([\w.]+)(\/([\w]+)\/?)*(\/[a-zA-Z0-9]+\.(\w+))*(\/([\w]+)\/?)*(\?(\w+=?[\w]*))*((&?\w+=?[\w]*))*$/';
 		if (strlen($html)<=400&&preg_match($pattern, $html)){
 	    	App::register('\Zjango\Laracurl\LaracurlServiceProvider');
 			$html=\Zjango\Laracurl\Facades\Laracurl::get($html)->body;
